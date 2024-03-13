35284919506d00487ac90b08140fe36f10efaec0
Rajeenthan <87084401+Rajeenthan@users.noreply.github.com>
2024-03-13 14:17:33 +0530

    updated
    updated

---
+++ 02.py
@@ -0,0 +1,46 @@
+i=0
+while i<10:
+    
+    i=i+1
+print(i)
+
+
+i=0
+while i<10:
+    print(i)
+    if i==5:
+        break
+    i=i+1
+
+    print()
+
+j=0
+while j<10:
+    print(i)
+    j=j+1
+    if j==5:
+        continue
+    print(j)
+
+print()
+
+for x in "University":
+    print(x)
+
+print()
+
+for x in range(5):
+    print(x)
+
+print()
+
+for x in range(2,9):
+    print(x)
+
+print()
+
+for x in range(1,10,2):
+    print(x)
+
+
+
