51148c8b37e11ba002bc0727b72a4e5f0b2f9c00
Rajeenthan <87084401+Rajeenthan@users.noreply.github.com>
2024-03-13 14:42:01 +0530

    Add files via upload

--- 02.py
+++ 02.py
@@ -1,6 +1,5 @@
 i=0
 while i<10:
-    
     i=i+1
 print(i)
 
@@ -42,5 +41,51 @@ print()
 for x in range(1,10,2):
     print(x)
 
+
+
+
+list1=['banana','apple','orange']
+print(list1)
+    
+print(type(list1))
+for x in list1:
+      print(x)
+
+      print()
+
+list1[1]='pineapple'
+print(list1)
+
+print()
+
+print(len(list1))
+      
+print(list1.pop())
+print(list1)
+
+list1.insert(1,'lemon')
+print(list1)
+
+tuple = ('car','cycle','plane')
+print(tuple)
+print(type(tuple))
+list1.extend(tuple)
+print(list1)
+
+set1={'a','b','c','d'}
+print(set1)
+print(type(set1))
+print(set1)
+
+
+set2={'a','b','c','d'}
+print(set2)
+print(type(set2))
+set2.add('e')
+print(set2)
+
+dil={'number' : 2}
+print(dil)
+      
 
 
